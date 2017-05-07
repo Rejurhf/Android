017b7eb992ac80a3bbefed3e43d67cd099441cae
Rejurhf <mziomek108@gmail.com>
2017-05-07 18:23:17 +0200

    Multiple Fragments

--- 002_Fragments/app/src/main/java/com/example/rejurhf/a002_fragments/MainActivity.java
+++ 002_Fragments/app/src/main/java/com/example/rejurhf/a002_fragments/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.rejurhf.a002_fragments;
 
+import android.content.res.Configuration;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int screenOrientation = getResources().getConfiguration().orientation;
+        if(screenOrientation == Configuration.ORIENTATION_PORTRAIT)
+            hideSidePanel();
+    }
+
+    private void hideSidePanel() {
+        View sidePanel = findViewById(R.id.side_panel);
+        if(sidePanel.getVisibility() == View.VISIBLE)
+            sidePanel.setVisibility(View.GONE);
     }
 }
---
+++ 002_Fragments/app/src/main/java/com/example/rejurhf/a002_fragments/SidePanelFragment.java
@@ -0,0 +1,20 @@
+package com.example.rejurhf.a002_fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Rejurhf on 07.05.2017.
+ */
+
+public class SidePanelFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_side_panel, container, false);
+    }
+}
